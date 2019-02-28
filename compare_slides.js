ab8d233287907a3347e18b07ca8f6750d3121fef
gwyn-art <gwyn.artstyle@gmail.com>
2019-02-28 20:38:59 +0200

    add comparing array of slides

--- compare_slides.js
+++ compare_slides.js
@@ -1,6 +1,8 @@
 const compareTwoSlides = (
   x = {tags: []}, 
   y = {tags: []}) => {
+    if (!x || !y) return 0;
+
     return Math.min(...[
       x.tags.filter(tag => y.tags.includes(tag)).length, 
       x.tags.filter(tag => !y.tags.includes(tag)).length,
@@ -9,5 +11,14 @@ const compareTwoSlides = (
 }
 
 const compareSlides = (slides) => {
-  const res = [];
+  return slides.reduce((res, x, i, arr) => 
+      [
+        ...res, 
+        {
+          left: compareTwoSlides(arr[i - 1], x),
+          right: compareTwoSlides(x, arr[i + 1])
+        }
+      ], 
+      []
+    );
 }
